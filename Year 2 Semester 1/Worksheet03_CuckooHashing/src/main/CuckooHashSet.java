b08ad058fbfe3e2e436b4739cdfe2c669d95ef85
Charungg <108029843+Charungg@users.noreply.github.com>
2024-10-21 19:11:54 +0100

    Add files via upload

--- Year 2 Semester 1/Worksheet03_CuckooHashing/src/main/CuckooHashSet.java
+++ Year 2 Semester 1/Worksheet03_CuckooHashing/src/main/CuckooHashSet.java
@@ -5,25 +5,29 @@ public class CuckooHashSet<K> implements Set<K>{
     private K[][] data;
     private int size;
 
+    private int capacity;
+
+
+
     public CuckooHashSet(int capacity){
         data = (K[][]) new Object[2][capacity];
-        size = capacity;
+        size = 0;
+        this.capacity = capacity;
     }
 
     public int hashFunction(int functionNumber, K key){
         int hashCode;
 
         if (functionNumber == 0){
-            hashCode = key.hashCode() % size;
+            hashCode = (key.hashCode()) % capacity;
             return hashCode;
         }
 
         else if (functionNumber == 1){
-            hashCode = ((key.hashCode())/size) % size;
+            hashCode = ((key.hashCode())/capacity) % capacity;
             return hashCode;
         }
 
-        System.out.println("no");
         return -1;
     }
 
@@ -34,13 +38,18 @@ public class CuckooHashSet<K> implements Set<K>{
         for (int i = 0; i<2; i++ ){
             hashValue = hashFunction(i,key);
             if (data[i][hashValue] == key){
-                System.out.println("Found dupe");
-                indexArray = new int[] {i,hashValue};
+                System.out.println(key + " is dupe");
+                System.out.println("Table " + i + " in index " + hashValue);
+                indexArray = new int[] {i, hashValue};
                 return indexArray;
+
+            }
+            else{
+                System.out.println(key + " does not exist within table " + i);
             }
         }
 
-        System.out.println("NULL");
+        System.out.println(key + " does not exist, returning null");
         return null;
     }
 
@@ -52,48 +61,49 @@ public class CuckooHashSet<K> implements Set<K>{
         return (position != null);
     }
 
+
     @Override
     public boolean put(K key) {
+        System.out.println("-------------------------------------------------------------");
         System.out.println("KEY = " + key);
-        System.out.println("KEY Table 0 = " + (key.hashCode() % size));
-        System.out.println("KEY Table 1 = " + ((key.hashCode()/size) % size));
-        System.out.println("Data = " + data);
+        System.out.println("KEY Table 0 = " + (key.hashCode() % capacity));
+        System.out.println("KEY Table 1 = " + ((key.hashCode()/capacity) % capacity));
 
         if (contains(key)){
             return false;
         }
 
         int currentTable = 0;
-        K currentKey = key;
         int evictions = 0;
+        int hashValue;
+        K currentKey = key;
+        K tempKey;
         boolean keyPlaced = false;
 
-        int hashValue;
-
         while (keyPlaced == false){
-             K tempKeyHolder;
-             hashValue = hashFunction(currentTable,currentKey);
-
-             if (data[currentTable][hashValue] == null){
-                 data[currentTable][hashValue] = key;
-                 size++;
-                 keyPlaced = true;
-             }
-
-             else if(data[currentTable][hashValue] != null){
-                 tempKeyHolder = data[currentTable][hashValue];
-                 data[currentTable][hashValue] = key;
-                 key = tempKeyHolder;
-                 evictions++;
-                 currentTable = (currentTable + 1) % 2;
-             }
-
-            if (evictions>size){
+            hashValue = hashFunction(currentTable,key);
+            if (evictions > size){
                 throw new IllegalStateException();
+            }
+
+            else if (data[currentTable][hashValue] == null){
+                data[currentTable][hashValue] = key;
+                keyPlaced = true;
+            }
+
+            else if(data[currentTable][hashValue] != null){
+                tempKey = data[currentTable][hashValue];
+                data[currentTable][hashValue] = key;
+                key = tempKey;
+                currentTable = (currentTable + 1) % 2;
+                evictions++;
             }
         }
 
+        size++;
         return true;
+
+
     }
 
     @Override
@@ -108,5 +118,23 @@ public class CuckooHashSet<K> implements Set<K>{
         }
 
         return false;
+    }
+
+    public void printData(){
+        System.out.print("\n");
+        System.out.println("Table 0:");
+        System.out.print("[");
+        for (int i =0; i<data[0].length; i++){
+            System.out.print((data[0][i]) + ", ");
+        }
+        System.out.println("]");
+
+        System.out.println("Table 1:");
+        System.out.print("[");
+        for (int i =0; i<data[0].length; i++){
+            System.out.print((data[1][i]) + ", ");
+        }
+        System.out.println("]");
+        System.out.print("\n");
     }
 }

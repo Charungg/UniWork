2c88caefb0cf3f9122fd26072f0bde7d7933e3f8
Charungg <108029843+Charungg@users.noreply.github.com>
2024-10-23 23:38:30 +0100

    Delete workshop_5 directory

--- workshop_5/roads/src/RoadType.java
+++
@@ -1,9 +0,0 @@
-/**
- * Types of roads:
- * M (Motorway)
- * B (Road)
- * U (Unclassfied)
- */
-public enum RoadType {
-    M, A, B, U;
-}
--- workshop_5/roads/src/SettlementType.java
+++
@@ -1,3 +0,0 @@
-public enum SettlementType {
-    HAMLET, VILLAGE, TOWN, CITY;
-}
